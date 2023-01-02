2b6806d93617335f3de2864960fdaf982d50e651
Damas2000 <119361513+Damas2000@users.noreply.github.com>
2023-01-02 11:06:08 +0300

    H1 Skrs güncellenmeyen tabloların düzeltilmesi

---
+++ Afinitem-AHBS/Entegrasyon/LstEgitimKurumlari.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Afinitem_AHBS.Entegrasyon
+{
+    public class LstEgitimKurumlari
+    {
+        public int kodu { get; set; }
+        public string adi { get; set; }
+        public int il_kodu { get; set; }
+        public int ilce_kodu { get; set; }
+    }
+}
--- Afinitem-AHBS/Staticler/Staticler.cs
+++ Afinitem-AHBS/Staticler/Staticler.cs
@@ -1,4 +1,5 @@
-﻿using SqlDeneme.Genel_Listeler;
+﻿using Afinitem_AHBS.Entegrasyon;
+using SqlDeneme.Genel_Listeler;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,9 @@ namespace SqlDeneme
 	{
 		public static List<Ilaclar> Ilac_lst { get; set; }
 		public static List<ICD_10> Icd10_lst { get; set; }
-		public static List<Lst_zehirlenmeye_neden_olan_etken> Zehirlenmeye_neden_olan_etken_lst { get; set; }
+        public static List<LstEgitimKurumlari> LstEgitimKurumlari { get; set; }
+
+        public static List<Lst_zehirlenmeye_neden_olan_etken> Zehirlenmeye_neden_olan_etken_lst { get; set; }
 		public static List<LstUlkeKodlari> Ulke_kodlari_lst { get; set; }
 		public static List<LstTumorunYeri> Tumorun_yeri_lst { get; set; }
 		public static List<LstSuttut> Suttut_lst { get; set; }
@@ -34,6 +37,8 @@ namespace SqlDeneme
 			Tumorun_yeri_lst = new List<LstTumorunYeri>();
 			Suttut_lst = new List<LstSuttut>();
 			Olası_vaka_tani_kriter_lst = new List<LstOlasiVakaTaniKriteri>();
-		}
+            LstEgitimKurumlari = new List<LstEgitimKurumlari>();
+
+        }
 	}
 }
